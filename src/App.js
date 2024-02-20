3de3f0ec256cf863fa8c5f9a187196fb277b5393
Nancy <ynancy030@gmail.com>
2024-02-20 13:52:40 +0530

    generator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Upload from "./components/Upload";
-import Generate from "./components/Generate";
 import Landing from "./components/Landing";
 import Footer from "./components/Footer";
 
@@ -9,7 +8,6 @@ export default function App() {
     <div className="bg-green-900 select-none">
       <Landing />
       <Upload />
-      <Generate />
       <Footer />
     </div>
   );
--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -1,17 +1,13 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Background from "../assets/background.svg";
-import { useScroll, useTransform, motion } from 'framer-motion';
-
+import { useScroll } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
 
 export default function Generate() {
+    const controls = useAnimation();
     const targetRef = useRef();
-    const { scrollYProgress } = useScroll({
-        target: targetRef,
-        offset: ["start end", "end center"]
-    })
-
+    const { scrollYProgress } = useScroll({ target: targetRef });
     const [randomText, setRandomText] = useState(Array.from({ length: 4 }, () => ""));
-    const xl = useTransform(scrollYProgress, [0.1, 0.25, 0.45, 0.9, 1], ["60%", "30%", "15%", "0%", "-140%"]);
 
     const ques = [
         {
@@ -36,6 +32,13 @@ export default function Generate() {
         },
     ];
 
+    useEffect(() => {
+        const animateDivs = async () => {
+            await controls.start('visible');
+        };
+        animateDivs();
+    }, [controls]);
+
     const generateRandomText = () => {
         const techWords = [
             'algorithm', 'API', 'authentication', 'backend', 'cloud computing',
@@ -57,11 +60,13 @@ export default function Generate() {
         setRandomText(newTextArray);
     };
 
-    return (
-        <div
-            ref={targetRef}
-            id="generator" className='h-auto bg-white selection-text relative mb-28'>
+    const variants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0 },
+    };
 
+    return (
+        <div ref={targetRef} id="generator" className='h-auto bg-white selection-text relative mb-28'>
             <div className='z-0 absolute '>
                 <img src={Background} alt="bg1" className=' w-[100rem] ' />
             </div>
@@ -72,32 +77,27 @@ export default function Generate() {
                     <div className='w-[400px] h-[3px] bg-black'></div>
                 </div>
 
-                <motion.div
-                    style={{ x }}
-                    className=''>
+                <div>
                     {ques.map((question, index) => (
-                        <div key={index} className={`${index % 2 == 1 ? "bg-[#15532E] hover:ring-green-300" : "bg-green-300 hover:ring-[#15532E]"} rounded px-10 p-6 m-3 overflow-hidden group  relative hover:bg-gradient-to-r hover:from-[#15532r] hover:to-[#15532w] text-white hover:ring-2 hover:ring-offset-2  transition-all ease-out duration-300`}>
+                        <motion.div
+                            key={index}
+                            custom={index}
+                            variants={variants}
+                            initial="hidden"
+                            animate={controls}
+                            transition={{ delay: index * 0.2 }}
+                            className={`${index % 2 == 1 ? "bg-[#15532E] hover:ring-green-300" : "bg-green-300 hover:ring-[#15532E]"} rounded px-10 p-6 m-3 overflow-hidden group  relative hover:bg-gradient-to-r hover:from-[#15532r] hover:to-[#15532w] text-white hover:ring-2 hover:ring-offset-2  transition-all ease-out duration-300`}>
                             <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-20 rotate-12 group-hover:-translate-x-40 ease"></span>
                             <span className="relative">
                                 <div className='flex w-[98%] justify-between text-lg  text-white'>
                                     <p className='w-[85%]'>{question.Ques}</p>
-
-                                    <button href="#_" className="hidden md:block relative md:px-4 md:py-1 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group text-md lg:text-lg" onClick={() => handleAskButtonClick(index)}>
-                                        <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-green-600 group-hover:w-full ease"></span>
-                                        <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-green-600 group-hover:w-full ease"></span>
-                                        <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-green-600 group-hover:h-full ease"></span>
-                                        <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-green-600 group-hover:h-full ease"></span>
-                                        <span className={`absolute inset-0 w-full h-full duration-300 delay-300 ${index % 2 == 0 ? "bg-green-300" : "bg-[#15532E]"}  opacity-0 group-hover:opacity-100`}></span>
-                                        <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">{question.btn}</span>
-                                    </button>
-
-                                    <button onClick={() => handleAskButtonClick(index)} className='block md:hidden'>{question.btn}</button>
+                                    <button onClick={() => handleAskButtonClick(index)} className=''>{question.btn}</button>
                                 </div>
                                 {randomText[index] && <p className='w-[85%]'>{randomText[index]}</p>}
                             </span>
-                        </div>
+                        </motion.div>
                     ))}
-                </motion.div>
+                </div>
             </div>
         </div>
     );
